b4da1be7713bd8e2ff145cfad92be46010747ea0
Hedge <trenthancock123@gmail.com>
2025-04-30 00:10:16 -0700

    test

--- commands.py
+++ commands.py
@@ -16,6 +16,12 @@ def setup_commands(bot):
             color=discord.Color.blue()
         )
         embed.add_field(name="Author", value=ctx.author.name, inline=False)
-        embed.add_field(name="Channel", value=ctx.channel.name, inline=False)
+
+        # Check if the channel is a DM or a guild channel
+        if isinstance(ctx.channel, discord.DMChannel):
+            embed.add_field(name="Channel", value="Direct Message", inline=False)
+        else:
+            embed.add_field(name="Channel", value=ctx.channel.name, inline=False)
+
         embed.set_footer(text="HedgeDiscordBot")
         await ctx.send(embed=embed)
